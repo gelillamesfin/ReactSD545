2f8a82f27cdae3fb878c542c566cd84ebe8a61f6
Gelilla Tafa <gelilamesfin13@yahoo.com>
2024-04-20 14:11:25 -0500

    music list map

---
+++ music/songList.tsx
@@ -0,0 +1,24 @@
+// import axios from 'axios'
+// import {useState,useEffect} from 'react'
+
+// export default function SongList (){
+//     const [songs,setsongs]=useState([]);
+
+//     useEffect(()=>{
+//         async function getSongs(){
+//             const response=await axios.get('http://localhost:3000/songs')
+//             setsongs(response.data)
+//         }
+//         getSongs()
+//     },[])
+    
+//     console.log(songs)
+//     return (
+// <div>
+
+//     <hr>from songList </hr>
+// </div>
+
+
+//     )
+// }
